43de64f73be5e40e61c728722d91b9b2bad5d188
Jacajacus <49621253+Jacajacus@users.noreply.github.com>
2020-02-22 08:47:18 +0100

    zadanie 1

---
+++ Zadanie 1/employee_managment/src/pl/gda/wsb/employee/EmployeeDemo.java
@@ -0,0 +1,12 @@
+package pl.gda.wsb.employee;
+
+import java.util.Date;
+
+public class EmployeeDemo {
+
+    public static void main(String args[]){
+    System.out.println("Hello " + args[0] + "!");
+        System.out.println("Aktualna data: " + new Date());
+
+    }
+}
